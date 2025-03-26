25a0459f69c32dfa4e78e587032fa2491d0b613c
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-25 20:43:24 -0400

    Added update! and setproperty! implementations to KnotPoint

--- src/methods_knot_point.jl
+++ src/methods_knot_point.jl
@@ -20,6 +20,22 @@ function Base.getindex(slice::KnotPoint, symb::Symbol)
     end
 end
 
+function Base.setproperty!(slice::KnotPoint, symb::Symbol, val::Any)
+    if symb in fieldnames(KnotPoint)
+        setfield!(slice, symb, val)
+    else
+        update!(slice, symb, val)
+    end
+end
+
+
+function Base.update!(slice::KnotPoint, symb::Symbol, data::AbstractVector{Float64})
+    @assert symb in slice.names
+    @assert size(data, 1) == (slice.components[symb].stop - slice.components[symb].start + 1)
+
+    Base.getproperty(slice, symb)[:] = data
+    return nothing
+end
 
 
 end
