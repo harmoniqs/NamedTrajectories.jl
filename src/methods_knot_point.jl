5156f1762fe5c34257ed53dbb4333d6592aceaf4
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-25 20:48:33 -0400

    Added setindex! to mirror getindex implementation for KnotPoint

--- src/methods_knot_point.jl
+++ src/methods_knot_point.jl
@@ -11,12 +11,12 @@ function Base.getproperty(slice::KnotPoint, symb::Symbol)
     end
 end
 
-function Base.getindex(slice::KnotPoint, symb::Symbol)
+function Base.getindex(slice::KnotPoint, symb::Symbol) # is this method redundant? i.e. should we just dispatch to getproperty?
     if symb in fieldnames(KnotPoint)
         return getfield(slice, symb)
     else
         indices = slice.components[symb]
-        return slice.data[indices]
+        return view(slice.data, indices)
     end
 end
 
@@ -26,6 +26,10 @@ function Base.setproperty!(slice::KnotPoint, symb::Symbol, val::Any)
     else
         update!(slice, symb, val)
     end
+end
+
+function Base.setindex!(slice::KnotPoint, symb::Symbol, val::Any)
+    setproperty!(slice, symb, val)
 end
 
 
