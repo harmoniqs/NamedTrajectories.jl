5fbba15d573423c4422df2cfff3036b5b46fc9e9
Andy Goldschmidt <agolschmidt11@gmail.com>
2025-06-28 15:44:29 -0500

    add indices to plot_name

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,18 +1,18 @@
 module Plotting
 
-export namedplot
-export namedplot!
+export plot_name
+export plot_name!
 
 """
-    namedplot(traj::NamedTrajectory, name::Symbol; kwargs...)
+    plot_name(traj::NamedTrajectory, name::Symbol; kwargs...)
 
 Plot a single component of a `NamedTrajectory` using Makie.
 
 The default plot type is `Series`. Series attributes can be passed as keyword arguments.
 
 """
-function namedplot end
+function plot_name end
 
-function namedplot! end
+function plot_name! end
 
 end
